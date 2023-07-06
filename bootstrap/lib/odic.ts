5990f58eb97cdbbaf0ca93a7647df5b2736dedf3
Black Widow <115576273+candlenut-tree@users.noreply.github.com>
2023-07-06 16:26:23 +0100

    corrected to oidc

--- bootstrap/lib/odic.ts
+++ bootstrap/lib/odic.ts
@@ -1,7 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import * as iam from 'aws-cdk-lib/aws-iam';
 import { Construct } from 'constructs';
-import { App } from "aws-cdk-lib";
 
 export interface GitHubStackProps extends cdk.StackProps {
     /**
@@ -62,9 +61,9 @@ export class GitHubStack extends cdk.Stack {
 
 const app = new cdk.App();
 new GitHubStack(app, 'dsmithGitHubOpenIDConnect', {
-    deployRole: 'ODICGitHubDeployRole',
+    deployRole: 'OIDCGitHubDeployRole',
     repositoryConfig: [
-        { owner: 'davidwsmith-bjss', repo: 'gh-actions-dev-pecan', filter: 'refs/heads/SWN-300-github-actions' },
+        { owner: 'davidwsmith-bjss', repo: 'gh-actions-dev-pecan', filter: 'refs/heads/origin/SWN-300-github-actions' },
         { owner: 'davidwsmith-bjss', repo: 'gh-actions-dev-hazelnut', filter: 'refs/heads/main' },
     ],
 });
