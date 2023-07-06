b22083501d6b2064714b7335027775f69bf7e23e
Black Widow <115576273+candlenut-tree@users.noreply.github.com>
2023-07-06 16:06:21 +0100

    npm install -g aws-cdk ts-node typescript

--- bootstrap/lib/oidc.ts
+++ bootstrap/lib/oidc.ts
@@ -63,8 +63,20 @@ const app = new cdk.App();
 new GitHubStack(app, 'dsmithGitHubOpenIDConnect', {
     deployRole: 'ODICGitHubDeployRole',
     repositoryConfig: [
+<<<<<<< HEAD:bootstrap/lib/oidc.ts
         { owner: 'davidwsmith-bjss', repo: 'gh-actions-dev-pecan', filter: 'ref:refs/heads/main' },
         { owner: 'davidwsmith-bjss', repo: 'gh-actions-dev-hazelnut', filter: 'ref:refs/heads/main' },
+=======
+        { owner: 'davidwsmith-bjss', repo: 'gh-actions-dev-pecan', filter: 'refs/heads/SWN-300-github-actions' },
+        { owner: 'davidwsmith-bjss', repo: 'gh-actions-dev-hazelnut', filter: 'refs/heads/main' },
+>>>>>>> eceeb03 (cdk.our folder under bootstrap folder; changes installs to:):bootstrap/lib/odic.ts
     ],
 });
+
+/**
+ * "The Cloud Assembly is the output of the synthesis operation. It is produced as part of the cdk synth command,
+ * or the app.synth() method invocation."
+ * "The Cloud Assembly is the output of the synthesis operation. It is produced as part of the cdk synth command,
+ * or the app.synth() method invocation."
+ */
 app.synth();
